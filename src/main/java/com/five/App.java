f1889b04a8197c9a7be9d6ca00b3fd6d73f538d5
Jiang Jian <1548063745@qq.com>
2021-12-27 16:47:32 +0800

    Init Project

---
+++ src/main/java/com/five/App.java
@@ -0,0 +1,13 @@
+package com.five;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
